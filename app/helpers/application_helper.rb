cf63eb055f0cbf356ec67087a074ce71f06f6d54
Cam Juarez <camjuar@gmail.com>
2013-10-02 21:04:33 -0700

    Fixed nav link behavior.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
-	def left_nav_button(link_text, link_path)
-	  class_name = current_page?(link_path) ? 'btn btn-success active' : 'btn btn-default'
-	  content_tag(:a, link_text, href: link_path, class: class_name)
+	def nav_newest_button(link_text, link_path)
+		class_name = ((current_page?(link_path) or current_page?(memes_path)) and params[:sort].nil?) ? 'btn btn-success active' : 'btn btn-default'
+		content_tag(:a, link_text, href: link_path, class: class_name)
 	end
+
+	def nav_sort_button(link_text, link_path, sort_by)
+		class_name = (current_page?(link_path) and params[:sort] == sort_by) ? 'btn btn-success active' : 'btn btn-default'
+		content_tag(:a, link_text, href: link_path, class: class_name)
+	end
+
 end
