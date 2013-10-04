09aab2dbedea55dba5e40a2b5fd4f231dc4126e9
Cam Juarez <camjuar@gmail.com>
2013-10-04 00:05:57 -0700

    Added Group model.

---
+++ db/migrate/20131004065835_create_groups.rb
@@ -0,0 +1,14 @@
+class CreateGroups < ActiveRecord::Migration
+  def change
+    create_table :groups do |t|
+      t.string :name
+      t.string :visibility
+      t.string :key
+    end
+
+    create_table :groups_users do |t|
+    	t.belongs_to :groups
+    	t.belongs_to :user
+    end
+  end
+end
