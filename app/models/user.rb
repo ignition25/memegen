3b13079cc7e2ef06ae5f71926e46a663e6012372
Cam Juarez <camjuar@gmail.com>
2013-10-09 20:13:23 -0700

    Added some validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :votes
 	has_and_belongs_to_many :groups
+	
+	validates_presence_of :username
+	validates_uniqueness_of :username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
