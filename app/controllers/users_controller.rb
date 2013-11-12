2aecef98421f7371da27fcf360c200a34ae6fffb
Cam Juarez <camjuar@gmail.com>
2013-11-11 20:30:13 -0800

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@favorites = []
-		@user.votes.each do |vote|
+		@user.votes.order("updated_at DESC").each do |vote|
 			if vote.value == 'up'
 				if !(meme = Meme.find_all_by_id(vote.meme_id).first).nil? && meme.user_id != @user.id
 					@favorites << meme
 				end
 			end
 		end
-		@memes = @user.memes
+		
+		@memes = @user.memes.order("created_at DESC")
 	end
 end
