04f1134ada4b482637f36aa5125b40bd1dd54c57
Cam Juarez <camjuar@gmail.com>
2014-05-06 21:32:12 -0700

    .

--- config/initializers/02_aws.rb
+++ config/initializers/02_aws.rb
@@ -11,6 +11,4 @@ ENV['S3_TEMPLATE_URL'] = 'https://memeosa-templates.s3.amazonaws.com/'
 AWS.config({
 		:access_key_id => ENV['ACCESS_KEY_ID'],
 		:secret_access_key => ENV['SECRET_ACCESS_KEY'],
-		#:access_key_id => 'AKIAJPXWZ3CRDM7QWZEA',
-		#:secret_access_key => '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',
 })
